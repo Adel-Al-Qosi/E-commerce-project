7e5bc6dc1e64e3a43a3443aa06412fec1323ef04
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2023-05-09 21:49:53 +0300

    change the method used for the navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,14 @@
 import NavBar from "./NavBar";
 import logo from "../images/logo.svg";
 import avatar from "../images/image-avatar.png";
-import { useState } from "react";
 import "../index.css";
 
 const Header = () => {
-  const [open, setOpen] = useState(false);
-  console.log(open);
+  
   return (
     <header className="flex header">
       <div className="flex">
-        <div className="hamburger">
-          <button value={open} onClick={(e) => {
-            e.preventDefault()
-            setOpen(!open)
-          }} className={"hamburger-button"}><span className="sr-only">menu</span></button>
-        </div>
-        <img className="logo" src={logo} alt="logo" />
+        <img src={logo} alt="logo"/>
         <NavBar />
       </div>
       <div className="flex">
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,43 @@
 import { NavLink } from "react-router-dom";
-import '../index.css'
+import "../index.css";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { useRef } from "react";
 
 const NavBar = () => {
+  const navRef = useRef()
+
+  const showNavBar = () => {
+    navRef.current.classList.toggle('responsive-nav')
+  }
+
   return (
-    <nav className="navbar">
-      <ul className="flex">
-        <li>
-          <NavLink to="/">Collections</NavLink>
-        </li>
-        <li>
-          <NavLink to="/Men">Men</NavLink>
-        </li>
-        <li>
-          <NavLink to="/Women">Women</NavLink>
-        </li>
-        <li>
-          <NavLink to="/About">About</NavLink>
-        </li>
-        <li>
-          <NavLink to="/Contact">Contact</NavLink>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav ref={navRef} className="navbar">
+        <ul className="flex">
+          <li>
+            <NavLink to="/">Collections</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Men">Men</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Women">Women</NavLink>
+          </li>
+          <li>
+            <NavLink to="/About">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Contact">Contact</NavLink>
+          </li>
+        </ul>
+        <button className="nav-btn nav-close-btn" onClick={showNavBar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavBar}>
+        <FaBars />
+      </button>
+    </>
   );
 };
 
