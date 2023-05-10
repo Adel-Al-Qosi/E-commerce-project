f64c0893e87f04ba7e06eab62fbef0825610875f
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2023-05-10 18:35:17 +0300

    style the cart container

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,7 +5,7 @@ const Cart = ({ isVisible }) => {
   return (
     <div className={`cart-container ${isVisible ? "visible" : ""}`}>
       <div className="cart-header">
-        <h2>Cart</h2>
+        <h2 className="cart-word">Cart</h2>
       </div>
       <div className="cart-body">
         {/* cart items here (maybe) */}
