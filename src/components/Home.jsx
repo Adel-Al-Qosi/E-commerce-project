d3b898d17bf658e7772a750df7e60b309512e7e2
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2023-05-12 14:04:33 +0300

    stuck. ask a question

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,12 @@ function Home() {
     <main className="home">
       <div className="gallery-container">
         <Gallery />
+        <div className="home--2"> 2 </div>
       </div>
-      <div className="home--2"> 2 </div>
-      <div className="home--3"> 3 </div>
-      <div className="home--4"> 4 </div>
+      <div className="info-container">
+        <div className="home--3"> 3 </div>
+        <div className="home--4"> 4 </div>
+      </div>
     </main>
   );
 }
